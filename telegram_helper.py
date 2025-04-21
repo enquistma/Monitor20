7cb7473f3f6978037e2a7872a5b2e570adeb585e
enquistma <36968064+enquistma@users.noreply.github.com>
2025-04-21 19:58:35 +0200

    Update telegram_helper.py

--- telegram_helper.py
+++ telegram_helper.py
@@ -3,9 +3,12 @@ import os
 
 def send_telegram_message(text):
     token = os.getenv("TELEGRAM_BOT_TOKEN")
-    chat_id = os.getenv("TELEGRAM_CHAT_ID")
+    chat_ids = os.getenv("TELEGRAM_CHAT_ID").split(',')
+
     url = f"https://api.telegram.org/bot{token}/sendMessage"
-    try:
-        requests.post(url, data={"chat_id": chat_id, "text": text})
-    except Exception as e:
-        print(f"[Telegram Error] {e}")
+
+    for chat_id in chat_ids:
+        try:
+            requests.post(url, data={"chat_id": chat_id.strip(), "text": text})
+        except Exception as e:
+            print(f"[Telegram Error] {e}")
